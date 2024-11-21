9cf629276e7fa98702d9722479f576dab1cf94f6
Merriet James <merrietjerry@ust-global.com>
2024-11-21 11:02:52 +0530

    doc-form

---
+++ components/doc-form/doc-form.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-doc-form',
+  templateUrl: './doc-form.component.html',
+  styleUrl: './doc-form.component.scss',
+})
+export class DocFormComponent {}
---
+++ components/doc-form/doc-form.module.ts
@@ -0,0 +1,10 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { DocFormComponent } from './doc-form.component';
+
+@NgModule({
+  declarations: [DocFormComponent],
+  imports: [CommonModule],
+  exports: [DocFormComponent],
+})
+export class DocFormModule {}
